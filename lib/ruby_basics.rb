d6d421ecc5617ca20a07abd5d8dde91ac96f107a
racquel giner <racquelginer828@gmail.com>
2017-05-25 17:27:47 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
 
 def assign_variable(value)
-  
+  var = value
 end
 
-def argue
-  
+def argue(arg)
+  return arg
 end
 
-def greeting
+def greeting(greetings, name)
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+  return "expert"
 end
 
-def pizza_party
-
+def pizza_party(toppings = "cheese")
+  return toppings
 end
